6eaf312a52a04db44335281222a7afedb0be5971
Pablo Tujillo Artunduaga <pabloartunduaga2020@gmail.com>
2026-04-08 17:40:26 -0500

    trabajo en clase

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,36 @@
+import { useForm } from 'react-hook-form'
+
+
+function RegisterPage() {
+    const {register, handleSubmit} = useForm();
+  return (
+    <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
+        <form onSubmit={handleSubmit((values) => {
+            console.log(values)
+        })}>
+            <input 
+            type="text"
+            {...register("username", {required: true})}
+            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+            placeholder='Username'
+            />
+            <input 
+            type="email"
+            {...register("email", {required: true})}
+            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+            placeholder='email@gmail.com'
+            />
+            <input 
+            type="password"
+            {...register("password", {required: true})}
+            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+            placeholder='Password'
+            />
+            <button type='Submit'>Register</button>
+        </form>
+    </div>
+
+  )
+}
+
+export default RegisterPage
