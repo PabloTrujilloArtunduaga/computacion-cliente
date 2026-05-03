0e3c3db68f816f032cd20c8c9efa7f96bd3ba53b
Pablo Tujillo Artunduaga <pabloartunduaga2020@gmail.com>
2026-05-03 11:33:03 -0500

    cambios
    cambios dashboard empleados

--- paginaLW/src/App.jsx
+++ paginaLW/src/App.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// IMPORTA EL CONTEXT DEL CARRITO
+// CONTEXT
 import { CarritoProvider } from "./context/CarritoContext";
 
+// COMPONENTES
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+// PÁGINAS
 import Inicio from "./pages/Inicio";
 import Nosotros from "./pages/Nosotros";
 import Contacto from "./pages/Contacto";
 import Productos from "./pages/Productos";
 import Carrito from "./pages/Carrito";
-import Dashboard from "./pages/Dashboard"; 
+
+// ADMIN / EMPLEADO
+import Admin from "./pages/admin/Admin.jsx";
+import Empleado from "./pages/empleados/empleados.jsx";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import ProductosCategoriasPage from "./pages/admin/ProductosAD.jsx";
+import UsuariosEmpleadosPage from "./pages/admin/UsuariosAD.jsx";
+import FacturasPage from "./pages/admin/Fact.jsx";
 
 function App() {
   return (
@@ -22,20 +32,25 @@ function App() {
 
         <div className="main-content">
           <Routes>
+            {/* PÚBLICO */}
             <Route path="/" element={<Inicio />} />
             <Route path="/productos" element={<Productos />} />
             <Route path="/nosotros" element={<Nosotros />} />
             <Route path="/contacto" element={<Contacto />} />
             <Route path="/carrito" element={<Carrito />} />
+
+            {/* EMPLEADO */}
+            <Route path="/empleado" element={<Empleado />} />
             <Route path="/dashboard-empleado" element={<Dashboard />} />
 
+            {/* ADMIN */}
             <Route path="/admin" element={<Admin />} />
-            <Route path="/empleado" element={<Empleado />} />
-            <Route path="*" element={<NotFound />} />
             <Route path="/admin/productos" element={<ProductosCategoriasPage />} />
             <Route path="/admin/usuarios" element={<UsuariosEmpleadosPage />} />
             <Route path="/admin/facturas" element={<FacturasPage />} />
-            
+
+            {/* 404 */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
 
